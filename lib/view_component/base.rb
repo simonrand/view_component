caefd276bf03ad4c1f30a58cf2d7d540b52630c9
Simon Rand <simonrand@gmail.com>
2020-04-04 22:57:10 +0100

    New approach to allow inline rendering on child components (including variant support)

--- lib/view_component/base.rb
+++ lib/view_component/base.rb
@@ -169,14 +169,7 @@ module ViewComponent
       end
 
       def inlined?
-        return false unless templates.empty?
-
-        # Only check if an inline render method has been defined on this class
-        # or on an ancestor, up to (but excluding) ViewComponent::Base
-        ancestor_classes = (ancestors[0...ancestors.index(ViewComponent::Base)] - included_modules)
-        return false unless ancestor_classes
-
-        ancestor_classes.any? { |ancestor_class| ancestor_class.instance_methods(false).grep(/^call/).present? }
+        inline_calls.present? && templates.empty?
       end
 
       def compile!
@@ -219,6 +212,8 @@ module ViewComponent
       end
 
       def variants
+        return inline_variant_templates if inlined?
+
         templates.map { |template| template[:variant] }
       end
 
@@ -249,6 +244,25 @@ module ViewComponent
       end
 
       private
+
+      def compiled_template(file_path)
+        handler = ActionView::Template.handler_for_extension(File.extname(file_path).gsub(".", ""))
+        template = File.read(file_path)
+
+        if handler.method(:call).parameters.length > 1
+          handler.call(self, template)
+        else
+          handler.call(OpenStruct.new(source: template, identifier: identifier, type: type))
+        end
+      end
+
+      def inline_calls
+        view_component_ancestors.flat_map { |ancestor| ancestor.instance_methods(false).grep(/^call/) }.uniq
+      end
+
+      def inline_variant_templates
+        inline_calls.reject { |call| call == :call }.map { |variant_call| variant_call.to_s.sub('call_', '').to_sym }
+      end
 
       def matching_views_in_source_location
         return [] unless source_location
@@ -291,15 +305,10 @@ module ViewComponent
           end
       end
 
-      def compiled_template(file_path)
-        handler = ActionView::Template.handler_for_extension(File.extname(file_path).gsub(".", ""))
-        template = File.read(file_path)
-
-        if handler.method(:call).parameters.length > 1
-          handler.call(self, template)
-        else
-          handler.call(OpenStruct.new(source: template, identifier: identifier, type: type))
-        end
+      def view_component_ancestors
+        # Fetch only ViewComponent ancestor classes
+        all_ancestors = ancestors
+        all_ancestors[0...all_ancestors.index(ViewComponent::Base)] - included_modules
       end
     end
 
